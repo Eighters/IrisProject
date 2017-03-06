7b16749fd92d865b7e99d640fe94c552382c1cfe
yanivk <yanivkoubbi@gmail.com>
2017-03-06 17:25:58 +0100

    Formulaire de création d'entreprise

--- src/Iris/CompanyBundle/Controller/CompanyController.php
+++ src/Iris/CompanyBundle/Controller/CompanyController.php
@@ -7,6 +7,9 @@ namespace Iris\CompanyBundle\Controller;
 use AppBundle\Entity\Company;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CompanyController extends Controller
 {
@@ -16,20 +19,47 @@ class CompanyController extends Controller
         
         // Création de l'entité Company
         $company = new Company();
-        $company->setRaisonSocial('CinovIt');
-        $company->setSiret("445666888259");
-        $company->setTelephone("0788050460");
+        
+        // On crée le FormBuilder grâce au service form factory
+        // On ajoute les champs de l'entité que l'on veut à notre formulaire
+        $form = $this->get('form.factory')->createBuilder(FormType::class, $company)
+            ->add('raisonSocial',      TextType::class)
+            ->add('siret',     TextType::class)
+            ->add('telephone',   TextType::class)
+            ->add('Enregistrer',      SubmitType::class)
+            ->getForm()
+        ;
+        
+        // Si la requête est en POST
+        if ($request->isMethod('POST')) {
+          // On fait le lien Requête <-> Formulaire
+          // À partir de maintenant, la variable $advert contient les valeurs entrées dans le formulaire par le visiteur
+          $form->handleRequest($request);
 
-        // On récupère l'EntityManager
-        $em = $this->getDoctrine()->getManager();
+          // On vérifie que les valeurs entrées sont correctes
+          // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+          if ($form->isValid()) {
+            // On enregistre notre objet $advert dans la base de données, par exemple
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($company);
+            $em->flush();
 
-        // Étape 1 : On « persiste » l'entité
-        $em->persist($company);
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
 
-        // Étape 2 : On déclenche l'enregistrement
-        $em->flush();
+            // On redirige vers la page de visualisation de l'annonce nouvellement créée
+            return $this->redirectToRoute('iris_company_fiche', array('id' => $company->getId()));
+          }
+        }
 
-        return $this->render('IrisCompanyBundle:Default:index.html.twig');
+        
+        // À partir du formBuilder, on génère le formulaire
+        //$form = $formBuilder->getForm();
+        
+        // On passe la méthode createView() du formulaire à la vue
+        // afin qu'elle puisse afficher le formulaire toute seule
+        return $this->render('IrisCompanyBundle:Default:creationEntreprise.html.twig', array(
+          'form' => $form->createView(),
+        ));
         
     }
     
