bdc457d21c27e56115b1d0e13d8f7467f6d486c7
yanivk <yanivkoubbi@gmail.com>
2017-03-09 23:15:41 +0100

    Surcharge de FosUserBundle
    L'intégration à l'application n'est pas encore au point mais la
    connexion a l'utilisateur fonctionne

---
+++ src/Iris/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Iris\UserBundle\Controller;
+
+// use utilisé pour la page Company
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+
+
+class RegistrationController extends BaseController
+{
+    
+    public function addAction(Request $request)
+    {
+        return $this->render('IrisUserBundle:Default:index.html.twig');
+        
+    }
+    
+    
+}
