483d9a0cae0960e3f27511598782e02dfd5cf9f1
yanivk <yanivkoubbi@gmail.com>
2017-03-10 13:38:07 +0100

    BundleUser

--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -3,7 +3,6 @@
 
 namespace AppBundle\Form;
 
-use AppBundle\Entity\Company;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -18,9 +17,6 @@ class RegistrationType extends AbstractType
     public function getParent()
     {
         return 'FOS\UserBundle\Form\Type\RegistrationFormType';
-
-        // Or for Symfony < 2.8
-        // return 'fos_user_registration';
     }
 
     public function getBlockPrefix()
