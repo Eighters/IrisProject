cca542b438d7ac0bbd892abdd253e449801facdf
yanivk <yanivkoubbi@gmail.com>
2017-04-21 14:08:35 +0200

    Accès refusé + modification formulaire user
    Privilège pour une page random.
    Page 403 personnalisée.
    Modification avec ajout du formulaire edition du profil.

--- src/Iris/UserBundle/Controller/UserController.php
+++ src/Iris/UserBundle/Controller/UserController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Security\Core\SecurityContext;
+
 /**
  * Description of UserController
  *
@@ -14,7 +17,12 @@ use AppBundle\Entity\User;
  */
 class UserController extends Controller{
     
+
     public function showAllAction(){
+
+
+    // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+    if ($this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
         $user = $this
         ->getDoctrine()
         ->getRepository('AppBundle:User')
@@ -27,5 +35,10 @@ class UserController extends Controller{
         
         return $this->render('IrisUserBundle:Default:listeUser.html.twig', 
                 array('user'  => $user ));
-    }    
+
+    }
+    else{
+        return $this->render( 'error403.html.twig' );
+    }  
+    }
 }
