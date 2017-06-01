c95af474d4cf6fed0586aeb11a2d795961baa77c
Rechad Sanehy <rechad.sanehy@gmail.com>
2017-06-01 16:58:55 +0200

    Correctif Sensio
    Suppression d'un use statement inutilisé
    Ajout de ligne de saut en fin de fichier

--- src/AppBundle/Form/Type/EnjeuxFormType.php
+++ src/AppBundle/Form/Type/EnjeuxFormType.php
@@ -19,7 +19,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class EnjeuxFormType extends AbstractType{
